91783446618da4da9a9521b30e21ffc8fc4ef27c
b-bond1 <blake.bond1@gmail.com>
2026-02-17 18:57:59 -0500

    Create Footer.js component with footer links and affiliate disclosure

---
+++ app/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h4>Links</h4>
+                <ul>
+                    <li><a href="https://example.com">Example Link 1</a></li>
+                    <li><a href="https://example.com">Example Link 2</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>Affiliate Disclosure</h4>
+                <p>
+                    As an affiliate, I earn from qualifying purchases. This does not affect your price and helps support our work.
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
